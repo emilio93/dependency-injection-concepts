476a91db9c31326c70da0e289310155addf44153
Emilio Rojas <emilio93@gmail.com>
2021-07-20 11:03:25 -0600

    Implement DI with multiple parameters using Unity

--- Driver.cs
+++ Driver.cs
@@ -5,16 +5,19 @@ namespace DependencyInjection
     public class Driver
     {
         private ICar _car = null;
+        private ICarKey _key = null;
 
-        public Driver(ICar car)
+        public Driver(ICar car, ICarKey key)
         {
             _car = car;
+            _key = key;
         }
 
         public void RunCar()
         {
             Console.WriteLine(
-                $"Running { _car.GetType().Name} - {_car.Run()} mile"
+                $"Running {_car.GetType().Name} " +
+                $"with {_key.GetType().Name} - {_car.Run()} mile "
             );
         }
     }
--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace DependencyInjection
         {
             IUnityContainer container = new UnityContainer();
             container.RegisterType<ICar, BMW>();
+            container.RegisterType<ICarKey, BMWKey>();
 
             var driver = container.Resolve<Driver>();
             driver.RunCar();
