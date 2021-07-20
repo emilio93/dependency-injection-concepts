750d6d8341945d4fd3e3098b52cb392b54003157
Emilio Rojas <emilio93@gmail.com>
2021-07-20 09:20:31 -0600

    Add classes A and B where class A is dependent on class B

---
+++ A.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class A
+    {
+        // class A is dependent on class B
+        // class B is a dependency of A
+        // in this case class A manages the lifetime of class B
+        B b;
+
+        public A()
+        {
+            b = new B();
+        }
+
+        public void Task1()
+        {
+            // do something here..
+            b.SomeMethod();
+            // do something here..
+        }
+    }
+}
